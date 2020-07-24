4f55d9bd408f30d4d48b4528591c71d109b8b483
Fabricio Garcia <fgarciazerda@gmail.com>
2020-07-23 22:29:02 -0500

    add docs

---
+++ docs/precache-manifest.1e0ded9afec5253c506cfae9e21aec3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187d5c3a1919b3e72ada9ac11973e00c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7ba84d3baab1a36c2c5",
+    "url": "/static/css/main.d2830e74.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ba84d3baab1a36c2c5",
+    "url": "/static/js/main.add2ed69.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
